73556a2ca3b6d50d211ced446c0a462b6763f098
xiaoheimiaomiao <925572702@qq.com>
2022-08-29 19:47:15 +0800

    51显示隐藏添加修改更新的界面

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { Card, Button, Table, Space } from 'antd';
-import { PlusOutlined } from '@ant-design/icons'
+import { Card, Button, Table, Space, Modal } from 'antd';
+import { PlusOutlined, ArrowRightOutlined } from '@ant-design/icons'
 import { reqCategorys } from '../../api/index'
 import './Category.less'
 export default function Category() {
@@ -11,9 +11,46 @@ export default function Category() {
   // 当前显示父类分类列表的名称
   const [parentName, setParentName] = useState('')
 
-  const title = "一级分类列表" + parentName
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalVisible2, setIsModalVisible2] = useState(false);
+  // 点击一级分类列表 跳转到一级分类列表
+  const shoeCategorys = () => {
+    setParentId('0')
+  }
+  // 显示添加框
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  // 添加
+  const addCategory = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  // 显示修改框
+  const showModal2 = () => {
+    setIsModalVisible2(true);
+  };
+  // 修改
+  const updateCategory = () => {
+    setIsModalVisible2(false);
+  };
+
+  const handleCancel2 = () => {
+    setIsModalVisible2(false);
+  };
+  const title = parentId === '0' ? '一级分类列表' : (
+    <span>
+      <Button type='link' onClick={shoeCategorys} >一级分类列表</Button>
+      <ArrowRightOutlined />
+      <span>{parentName}</span>
+    </span>
+  )
+
   const extra = (
-    <Button>
+    <Button onClick={showModal} >
       <PlusOutlined />
       添加
     </Button>
@@ -23,6 +60,7 @@ export default function Category() {
     setParentId(categorys._id)
     setParentName(categorys.name)
   }
+
   const columns = [
     {
       title: '分类名称',
@@ -35,38 +73,48 @@ export default function Category() {
       key: 'action',
       render: (categorys) => (
         <Space size="middle">
-          <Button type='link' >修改分类</Button>
-          <Button type='link' onClick={() => { showSubCategorys(categorys) }}>查看子分类</Button>
+          <Button type='link' onClick={showModal2} >修改分类</Button>
+          {parentId === '0' ? <Button type='link' onClick={() => { showSubCategorys(categorys) }}>查看子分类</Button> : null}
         </Space>
       ),
     },
   ]
-
-
-
-
   useEffect(() => {
     reqCategorys(parentId).then(result => {
       // console.log(result)
       const list = result.data
       // console.log(list)
-
       setCategorys(list)
-
     })
   }, [parentId])
-
   return (
-
-    <Card title={title} extra={extra} >
-      <Table
-        // 是否展示外边框和列边框
-        bordered={true}
-        columns={columns}
-        dataSource={categorys}
-        pagination={{ pageSize: 5 }}
-      // scroll={{y:300}}
-      />
-    </Card>
+    <>
+      <Modal title="添加分类"
+        visible={isModalVisible}
+        onOk={addCategory}
+        onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      <Modal title="更新分类"
+        visible={isModalVisible2}
+        onOk={updateCategory}
+        onCancel={handleCancel2}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+      <Card title={title} extra={extra} >
+        <Table
+          // 是否展示外边框和列边框
+          bordered={true}
+          columns={columns}
+          dataSource={categorys}
+          pagination={{ pageSize: 5 }}
+        // scroll={{y:300}}
+        />
+      </Card>
+    </>
   )
 }
