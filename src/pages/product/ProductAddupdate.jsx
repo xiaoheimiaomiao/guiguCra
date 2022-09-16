ad4e6ecc9e667c8aadcdf762b246b6c1d35a5955
xiaoheimiaomiao <925572702@qq.com>
2022-09-16 21:06:19 +0800

    74productAdd组件默认显示分类1

--- src/pages/product/ProductAddupdate.jsx
+++ src/pages/product/ProductAddupdate.jsx
@@ -5,7 +5,8 @@ import {
   Button,
   Form,
   Input,
-  message,
+
+  Modal,
   Upload,
   Cascader
 } from 'antd'
@@ -14,99 +15,45 @@ import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import { reqCategorys } from '../../api/index'
 import { useEffect } from 'react';
 const { TextArea } = Input;
-// 上传图片
-const getBase64 = (img, callback) => {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
-};
 
-const beforeUpload = (file) => {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
 
-  if (!isJpgOrPng) {
-    message.error('You can only upload JPG/PNG file!');
-  }
+    reader.onload = () => resolve(reader.result);
 
-  const isLt2M = file.size / 1024 / 1024 < 2;
+    reader.onerror = (error) => reject(error);
+  });
 
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-
-  return isJpgOrPng && isLt2M;
-};
 
 export default function ProductAddupdate() {
   const navigate = useNavigate()
   const [loading, setLoading] = useState(false);
   const [imageUrl, setImageUrl] = useState();
   const [options, setOptions] = useState([]);
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [previewTitle, setPreviewTitle] = useState('');
+  const [fileList, setFileList] = useState([ ]);
 
-  const initOptions = (categorys) => {
-    // 根据category数组 生成option数组
-    const options = categorys.map((item) => ({
-      value: item._id,
-      label: item.name,
-      isLeaf: false,
-    }))
-    // 更新option状态
-    setOptions(options)
-  }
+  const handleCancel = () => setPreviewOpen(false);
 
-  // 获取一级、二级分类列表并显示
-  const getCategorys = async (parentId) => {
-    const result = await reqCategorys(parentId)
-    if (result.status === 0) {
-      const categorys = result.data
-      // console.log(categorys)
-      initOptions(categorys)
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
     }
-  }
 
-  const loadData = (selectedOptions) => {
-    // 选择的option对象
-    const targetOption = selectedOptions[selectedOptions.length - 1];
-    targetOption.loading = true; // load options lazily
-
-    // 根据选中的分类，请求二级列表
-
-    // 模拟请求异步二级列表数据，并更新
-    setTimeout(() => {
-      targetOption.loading = false;
-      targetOption.children = [
-        {
-          label: `${targetOption.label} Dynamic 1`,
-          value: 'dynamic1',
-        },
-        {
-          label: `${targetOption.label} Dynamic 2`,
-          value: 'dynamic2',
-        },
-      ];
-      setOptions([...options]);
-    }, 1000);
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
   };
 
-  // 上传图片
-  const handleChange = (info) => {
-    if (info.file.status === 'uploading') {
-      setLoading(true);
-      return;
-    }
-
-    if (info.file.status === 'done') {
-      // Get this url from response in real world.
-      getBase64(info.file.originFileObj, (url) => {
-        setLoading(false);
-        setImageUrl(url);
-      });
-    }
-  };
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
 
   const uploadButton = (
     <div>
-      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <PlusOutlined />
       <div
         style={{
           marginTop: 8,
@@ -116,10 +63,80 @@ export default function ProductAddupdate() {
       </div>
     </div>
   );
+  const initOptions = (categorys) => {
+    // 根据category数组 生成option数组
+    const options = categorys.map((item) => ({
+      value: item._id,
+      label: item.name,
+      isLeaf: false,
+    }))
+    // 更新option状态
+    // console.log(options)
+    setOptions(options)
+
+  }
+
+  // 获取一级、二级分类列表并显示
+  const getCategorys = async (parentId) => {
+    const result = await reqCategorys(parentId)
+    if (result.status === 0) {
+      const categorys = result.data
+
+      if (parentId === '0') {
+        initOptions(categorys)
+        // console.log(categorys)
+      } else {
+        // 二级列表
+        // 当前async函数返回的promise对象就会成功 且value为categorys
+        return categorys
+      }
+
+    }
+  }
+
+  // 用于加载下一级列表的回调函数
+  const loadData = async (selectedOptions) => {
+    // 选择的option对象
+    const targetOption = selectedOptions[selectedOptions.length - 1];
+    targetOption.loading = true; // load options lazily
+
+    // 根据选中的分类，请求二级列表
+    const subCategorys = await getCategorys(targetOption.value)
+    targetOption.loading = false;
+    if (subCategorys && subCategorys.length > 0) {
+      const childOptions = subCategorys.map(item => ({
+        value: item._id,
+        label: item.name,
+        isLeaf: true
+      }))
+      // 关联到当前option上
+      targetOption.children = childOptions
+    } else {
+      // 如果当前选中的分类没有二级分i类
+      targetOption.isLeaf = true
+
+    }
+    setOptions([...options]);
+    // 模拟请求异步二级列表数据，并更新
+
+  };
+
+
 
 
   const onFinish = (values) => {
     console.log('Success:', values);
+    // 1收集数据 并封装成product对象
+    const { name, desc, price, categoryIds } = values
+    let pCategoryId, categoryId
+    if (categoryIds.length === 1) {
+      pCategoryId = '0'
+      categoryId = categoryIds[0]
+    } else {
+      pCategoryId = categoryIds[0]
+      categoryId = categoryIds[1]
+    }
+
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -219,26 +236,29 @@ export default function ProductAddupdate() {
 
           >
             <Upload
-              name="avatar"
-              listType="picture-card"
-              className="avatar-uploader"
-              showUploadList={false}
               action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              beforeUpload={beforeUpload}
+              listType="picture-card"
+              fileList={fileList}
+              onPreview={handlePreview}
               onChange={handleChange}
             >
-              {imageUrl ? (
-                <img
-                  src={imageUrl}
-                  alt="avatar"
-                  style={{
-                    width: '100%',
-                  }}
-                />
-              ) : (
-                uploadButton
-              )}
+              {fileList.length >= 8 ? null : uploadButton}
             </Upload>
+            <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+              <img
+                alt="example"
+                style={{
+                  width: '100%',
+                }}
+                src={previewImage}
+              />
+            </Modal>
+
+          </Form.Item>
+          <Form.Item
+            label="商品详情"
+          >
+            商品详情
           </Form.Item>
           <Form.Item
             wrapperCol={{
